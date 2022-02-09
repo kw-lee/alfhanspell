2ffd57b3ec103f069cc756ce0980919ecf49b453
Kyeongwon Lee <kwlee1718@gmail.com>
2022-02-09 12:25:25 +0900

    add todo: run workflow without python3

--- make.sh
+++ make.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# todo: run workflow without python3
+# pyinstaller --additional-hooks-dir=workflow workflow/naver_spellcheck.py
+
 cd workflow
+rm -rf __pycache__
 zip -r ../NaverSC.alfredworkflow .
