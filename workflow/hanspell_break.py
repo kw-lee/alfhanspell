1afdf72d1ed28518d1d48bad0fe4fe27929cde29
Kyeongwon Lee <kwlee1718@gmail.com>
2022-04-24 16:40:50 +0900

    change spellchecker url

--- workflow/hanspell_break.py
+++ workflow/hanspell_break.py
@@ -12,19 +12,20 @@ import xml.etree.ElementTree as ET
 
 from workflow import __version__
 from hanspell.response import Checked
-from hanspell.constants import base_url
 from hanspell.constants import CheckResult
+
+base_url = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy'
 
 _agent = requests.Session()
 PY3 = sys.version_info[0] == 3
 
 
 def _remove_tags(text):
-    text = u'<content>{}</content>'.format(text).replace('<br>','\n')
+    text = u"<content>{}</content>".format(text).replace("<br>","\n")
     if not PY3:
-        text = text.encode('utf-8')
+        text = text.encode("utf-8")
 
-    result = ''.join(ET.fromstring(text).itertext())
+    result = "".join(ET.fromstring(text).itertext())
 
     return result
 
@@ -45,71 +46,76 @@ def check(text):
         return Checked(result=False)
 
     payload = {
-        '_callback': 'window.__jindo2_callback._spellingCheck_0',
-        'q': text
+        "_callback": "window.__jindo2_callback._spellingCheck_0",
+        "q": text,
+        "color_blindness": 0
     }
 
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-        'referer': 'https://search.naver.com/',
+        "Host": "m.search.naver.com",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.9 Safari/537.36",
+        "referer": "https://search.naver.com/",
+        "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Accept": "*/*"
     }
 
     start_time = time.time()
     r = _agent.get(base_url, params=payload, headers=headers)
+
     passed_time = time.time() - start_time
 
     r = r.text[42:-2]
 
     data = json.loads(r)
-    html = data['message']['result']['html']
+    html = data["message"]["result"]["html"]
     result = {
-        'result': True,
-        'original': text,
-        'checked': _remove_tags(html),
-        'errors': data['message']['result']['errata_count'],
-        'time': passed_time,
-        'words': OrderedDict(),
+        "result": True,
+        "original": text,
+        "checked": _remove_tags(html),
+        "errors": data["message"]["result"]["errata_count"],
+        "time": passed_time,
+        "words": OrderedDict(),
     }
 
     # 띄어쓰기로 구분하기 위해 태그는 일단 보기 쉽게 바꿔둠.
     # ElementTree의 iter()를 써서 더 좋게 할 수 있는 방법이 있지만
     # 이 짧은 코드에 굳이 그렇게 할 필요성이 없으므로 일단 문자열을 치환하는 방법으로 작성.
-    html = html.replace('<span class=\'green_text\'>', '<green>') \
-               .replace('<span class=\'red_text\'>', '<red>') \
-               .replace('<span class=\'purple_text\'>', '<purple>') \
-               .replace('<span class=\'blue_text\'>', '<blue>') \
-               .replace('</span>', '<end>')
-    items = html.split(' ')
+    html = html.replace("<span class=\"green_text\">", "<green>") \
+               .replace("<span class=\"red_text\">", "<red>") \
+               .replace("<span class=\"purple_text\">", "<purple>") \
+               .replace("<span class=\"blue_text\">", "<blue>") \
+               .replace("</span>", "<end>")
+    items = html.split(" ")
     words = []
-    tmp = ''
+    tmp = ""
     for word in items:
-        if tmp == '' and word[:1] == '<':
-            pos = word.find('>') + 1
+        if tmp == "" and word[:1] == "<":
+            pos = word.find(">") + 1
             tmp = word[:pos]
-        elif tmp != '':
-            word = u'{}{}'.format(tmp, word)
+        elif tmp != "":
+            word = u"{}{}".format(tmp, word)
         
-        if word[-5:] == '<end>':
-            word = word.replace('<end>', '')
-            tmp = ''
+        if word[-5:] == "<end>":
+            word = word.replace("<end>", "")
+            tmp = ""
 
         words.append(word)
 
     for word in words:
         check_result = CheckResult.PASSED
-        if word[:5] == '<red>':
+        if word[:5] == "<red>":
             check_result = CheckResult.WRONG_SPELLING
-            word = word.replace('<red>', '')
-        elif word[:7] == '<green>':
+            word = word.replace("<red>", "")
+        elif word[:7] == "<green>":
             check_result = CheckResult.WRONG_SPACING
-            word = word.replace('<green>', '')
-        elif word[:8] == '<purple>':
+            word = word.replace("<green>", "")
+        elif word[:8] == "<purple>":
             check_result = CheckResult.AMBIGUOUS
-            word = word.replace('<purple>', '')
-        elif word[:6] == '<blue>':
+            word = word.replace("<purple>", "")
+        elif word[:6] == "<blue>":
             check_result = CheckResult.STATISTICAL_CORRECTION
-            word = word.replace('<blue>', '')
-        result['words'][word] = check_result
+            word = word.replace("<blue>", "")
+        result["words"][word] = check_result
 
     result = Checked(**result)
 
